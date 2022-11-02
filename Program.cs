9a7f9a246abbf0321c655b2e0d9e1f8c07a46b93
Artem-GB <115541735+Artem-GB@users.noreply.github.com>
2022-11-03 01:30:37 +0500

    Add files via upload
    Написал программу для решения задачи и залил ее на github

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3
+// символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении 
+// не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+int[] RandomArray(int size, int minValue, int maxValue)
+{
+    int[] myArray = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        myArray[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return myArray;
+}
+
+
+void ShowArray(int[] array)
+{
+    Console.WriteLine("Случайно заданный массив -> ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void ReducedArray(int[] array)
+{
+    Console.WriteLine("Получившийся массив -> ");
+    for(int i = 0; i <= 2; i++)
+    {
+       Console.Write(array[i] + " ");
+    }
+    
+}
+
+Console.WriteLine("Введите число от 5 до 100: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [] myArray = RandomArray(a, min, max);
+ShowArray(myArray);
+Console.WriteLine();
+ReducedArray(myArray);
